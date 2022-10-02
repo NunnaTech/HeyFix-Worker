e77c4815373d4b06b06b54638d8228dd2ed71f24
Jackeline Aguas <20193tn001@utez.edu.mx>
2022-10-01 19:23:01 -0500

    add view service

---
+++ app/src/main/java/com/pandadevs/heyfix_worker/ServiceActivity.kt
@@ -0,0 +1,11 @@
+package com.pandadevs.heyfix_worker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ServiceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_service)
+    }
+}
